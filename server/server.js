3c46808ddc715caf536ae5219fb1db9c9709301f
Arman <contact.armankar@gmail.com>
2025-02-13 21:08:46 +0530

    Resolved Merge Conflicts

--- server/server.js
+++ server/server.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
 const app = express();
 
 dotenv.config();
+connectDB();
+
 app.use(express.json());
 
 app.get('/ping', (req, res) => {
-  try {
-    res.status(200).json({ message: 'Server is up and running' });
-  } catch (error) {
-    res.status(500).json({ message: 'Internal server error' });
-  }
+	try {
+		res.status(200).json({ message: 'Server is up and running' });
+	} catch (error) {
+		res.status(500).json({ message: 'Internal server error' });
+	}
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
