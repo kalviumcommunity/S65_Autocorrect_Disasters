748591014cb903b2e977fa15b7db89edc6455182
Arman <contact.armankar@gmail.com>
2025-02-13 22:02:14 +0530

    Sub-optimal Backend Setup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
-<<<<<<< HEAD
 const connectDB = require('./config/database');
 const userRoutes = require('./routes/user-route');
 const imageRoutes = require('./routes/image-route');
-=======
 const connectDB = require('./config/db');
->>>>>>> eca112168ce40be86580ac8c696c9b51a7bc2696
 
 const app = express();
 
@@ -16,19 +13,11 @@ connectDB();
 app.use(express.json());
 
 app.get('/ping', (req, res) => {
-<<<<<<< HEAD
     try {
         res.status(200).json({ message: 'Server is up and running' });
     } catch (error) {
         res.status(500).json({ message: 'Internal server error' });
     }
-=======
-	try {
-		res.status(200).json({ message: 'Server is up and running' });
-	} catch (error) {
-		res.status(500).json({ message: 'Internal server error' });
-	}
->>>>>>> eca112168ce40be86580ac8c696c9b51a7bc2696
 });
 
 app.use('/', userRoutes);
@@ -37,9 +26,5 @@ app.use('/', imageRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-<<<<<<< HEAD
     console.log(`Server is running on port ${PORT}`);
-=======
-	console.log(`Server is running on port ${PORT}`);
->>>>>>> eca112168ce40be86580ac8c696c9b51a7bc2696
 });
